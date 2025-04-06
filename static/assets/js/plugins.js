645065d5702903db990d61ed0106c251af97d092
Roman Yahin <roman.nadym@yandex.ru>
2025-04-06 22:46:31 +0300

    Добавлены стили и скрипты для UI velzon v 4.2.0

---
+++ static/assets/js/plugins.js
@@ -0,0 +1,5 @@
+if (document.querySelectorAll("[toast-list]") || document.querySelectorAll("[data-choices]") || document.querySelectorAll("[data-provider]")) {
+    document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'><\/script>");
+    document.writeln(`<script type='text/javascript' src='${choicesScript}'><\/script>`);
+    document.writeln(`<script type='text/javascript' src='${flatpickrScript}'><\/script>`);
+}
