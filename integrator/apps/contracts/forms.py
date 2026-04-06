5a488c6640cc25e79fbf54c03767dcdc639156fa
Roman Yahin <roman.nadym@yandex.ru>
2026-04-07 00:30:55 +0300

    Изменена логика обработки конечного пользователя организация на странице редактирования контрактов

--- integrator/apps/contracts/forms.py
+++ integrator/apps/contracts/forms.py
@@ -3,8 +3,8 @@ from django.forms.models import inlineformset_factory
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django_select2 import forms as s2forms
-from contracts.models import SupportLevelModel, ContractModel, ContractEquipmentModel
-
+from contracts.models import SupportLevelModel, ContractModel, ContractEquipmentModel, ContractEndUser
+from accounts.models import OrganizationModel
 User = get_user_model()
 
 class LevelForm(forms.ModelForm):
@@ -41,16 +41,36 @@ class ClientMultipleWidget(s2forms.ModelSelect2Widget):
 class ContractForm(forms.ModelForm):
     # client = forms.ModelChoiceField(label = 'Заказчик', queryset = User.objects.filter(Q(is_active = True) & ~Q(email = 'serindework@mail.ru')))
     # end_user = forms.ModelChoiceField(label = 'Конечный пользователь', queryset = User.objects.filter(Q(is_active = True) & ~Q(email = 'serindework@mail.ru')))
+    # Теперь это поле выбора ОРГАНИЗАЦИИ, а не пользователя
+    end_user_organization = forms.ModelChoiceField(
+        label='Конечный пользователь (Организация)',
+        queryset=OrganizationModel.objects.all(),
+        widget=s2forms.ModelSelect2Widget(
+            model=OrganizationModel,
+            search_fields=['name__icontains'],
+            attrs={'class': 'form-select', 'data-minimum-input-length': 0}
+        ),
+        required=False
+    )
     class Meta:
         model = ContractModel
+        # Исключаем end_users из автоматической обработки, так как мы добавили кастомное поле end_user
+        exclude = ('end_users','organization')
         fields = '__all__'
         widgets = {
             'client': ClientWidget(attrs = {'data-minimum-input-length': 0, 'class': 'js-example-basic-single form-select'}, queryset = User.objects.filter(Q(groups__name = 'Заказчик') & Q(is_active = True))),
-            'end_users': ClientMultipleWidget(attrs = {'data-minimum-input-length': 0, 'multiple': 'multiple'}, queryset = User.objects.filter(Q(groups__name = 'Заказчик') & Q(is_active = True))),
             # 'client': forms.Select(queryset = User.objects.filter(Q(is_active = True) & ~Q(email = 'serindework@mail.ru'))),
             'signed': forms.DateInput(attrs = {'class': 'date'}),
             'enddate': forms.DateInput(attrs = {'class': 'date'}),
         }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            # Подтягиваем организацию из таблицы связей
+            link = ContractEndUser.objects.filter(contractmodel=self.instance).first()
+            if link and link.organization_id:
+                self.initial['end_user_organization'] = link.organization_id
 
 class ContractDeleteForm(forms.ModelForm):
     class Meta:
